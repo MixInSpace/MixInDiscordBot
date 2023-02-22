199f6f4c162618efd0acdc0c0fcefedcf2f43789
MixInSpace <mi.maruas@yandex.ru>
2023-02-22 21:48:48 +0800

    fix

--- commands/prefix/core/like.js
+++ commands/prefix/core/like.js
@@ -2,7 +2,7 @@ module.exports = {
     name:'like',
     description:"Like the replyed message",
     run:async(client, message, args)=>{
-        if(!message.reference){
+        if(message.reference){
             const repliedTo = await message.fetchReference()
         repliedTo.react('👍')
             .then(await message.delete())
